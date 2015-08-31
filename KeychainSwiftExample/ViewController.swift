b7424187449d2fec2b5a4891ca4e43f2084468c1
iPhonig <bhonig@iphonig.com>
2015-08-30 21:19:44 -0400

    Initial Commit

---
+++ KeychainSwiftExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeychainSwiftExample
+//
+//  Created by Ben Honig on 8/30/15.
+//  Copyright (c) 2015 iPhonig, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
